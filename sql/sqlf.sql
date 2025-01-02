84938027a8dd46e5305e1a450ff049890e95fff8
ShadowMainCell <yuralaptenkov@gmail.com>
2025-01-03 00:11:34 +0300

    sqlf.sql

---
+++ sql/sqlf.sql
@@ -0,0 +1,6 @@
+create database "SQL"
+create table "SQL Files"
+create data "sqlite3.dll" REAL DEFAULT "sql.dsql
+create database "TeX"
+create table "TeX Files"
+create data "dbloader.cbx" REAL DEFAULT "dbloader.cbx"
